cc317825c8262e01210b9a73bbd00098b68462d7
John Sun <ruohansun2009@gmail.com>
2025-10-04 01:01:27 +0000

    Update siteProps with user information and enhance social icon styles

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "John Sun",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "john@example.com",
+  gitHub: "ruohansun",
+  instagram: "ruohansun",
+  linkedIn: "ruohansun",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ruohansun",
+  youTube: "Ruohansun",
 };
 
 const primaryColor = "#4E567E";
